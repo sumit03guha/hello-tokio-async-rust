bce4b4dec9650a2a899ac45d6460ebe53aab0933
Sumit Guha <sumit03guha@gmail.com>
2025-08-30 02:54:11 +0530

    refactor delay example to use tracing for logging instead of println

--- src/bin/delay.rs
+++ src/bin/delay.rs
@@ -1,43 +1,55 @@
 use std::{task::Poll, thread, time::Duration};
-
 use tokio::time::Instant;
+
+use hello_tokio_async_rust::utils::get_logger;
 
 struct Delay {
     when: Instant,
+    spawned: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    println!("Let's go!");
+    let sub = get_logger();
+    tracing::subscriber::set_global_default(sub).unwrap();
+
+    tracing::info!("Let's go!");
+
     let when = Instant::now() + Duration::from_secs(3);
-    let delay = Delay { when };
+    let delay = Delay {
+        when,
+        spawned: false,
+    };
 
     let res = delay.await;
-    println!("res : {}", res);
+    tracing::info!("res : {}", res);
 
-    println!("Done")
+    tracing::info!("Done")
 }
 
 impl Future for Delay {
     type Output = &'static str;
     fn poll(
-        self: std::pin::Pin<&mut Self>,
+        mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
         if self.when <= Instant::now() {
-            println!("Ready");
+            tracing::info!("Ready");
             Poll::Ready("Ready")
         } else {
-            let when = self.when;
-            let waker = cx.waker().clone();
-            thread::spawn(move || {
-                if when > Instant::now() {
-                    println!("Going to sleep");
-                    thread::sleep(when - Instant::now());
-                }
-                waker.wake();
-            });
-            println!("Pending");
+            if !self.spawned {
+                self.spawned = true;
+                let when = self.when;
+                let waker = cx.waker().clone();
+                thread::spawn(move || {
+                    if when > Instant::now() {
+                        tracing::info!("Going to sleep");
+                        thread::sleep(when - Instant::now());
+                    }
+                    waker.wake();
+                });
+            }
+            tracing::info!("Pending");
             Poll::Pending
         }
     }
