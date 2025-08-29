9f31f0d5ac6f67ac4815d1da29d41aad16498aef
Sumit Guha <sumit03guha@gmail.com>
2025-08-30 02:53:57 +0530

    add utils module with logger implementation using tracing

---
+++ src/utils.rs
@@ -0,0 +1,17 @@
+use tracing::Subscriber;
+
+pub fn get_logger() -> impl Subscriber {
+    tracing_subscriber::fmt()
+        // Use a more compact, abbreviated log format
+        .compact()
+        // Display source code file paths
+        .with_file(true)
+        // Display source code line numbers
+        .with_line_number(true)
+        // Display the thread ID an event was recorded on
+        .with_thread_ids(true)
+        // Don't display the event's target (module path)
+        .with_target(false)
+        // Build the subscriber
+        .finish()
+}
